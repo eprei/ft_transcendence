dd29a9f3ef74e38fa13f69b30c16c1a151b74e60
|°_°| <grivel.theo@protonmail.com>
2023-05-17 16:50:45 +0200

    set the background to the whole page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
     return (
         <>
             <div className="container" style={containerStyle}>
+            <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
@@ -40,6 +41,7 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            </div>
         </>
     )
 }
