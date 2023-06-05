3da545bb7d6118637cd9a576757091bd540be0f1
|°_°| <grivel.theo@protonmail.com>
2023-06-05 17:51:38 +0200

    SignInForm
    see:
    https://react-hook-form.com/get-started#Quickstart

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,12 @@
+import SignInForm from '../components/signIn/Form'
+
 const Product = () => {
-    return <h1>Sign in</h1>
+    return (
+        <>
+            <h1>Sign in</h1>
+            <SignInForm />
+        </>
+    )
 }
 
 export default Product
