2fdbdc8286ce98c4e73fb64d66216bd7ce64e48b
|°_°| <grivel.theo@protonmail.com>
2023-06-06 11:55:37 +0200

    create form

---
+++ frontend/src/components/signIn/Form.tsx
@@ -0,0 +1,12 @@
+import styles from './Form.module.css'
+
+const Form = () => {
+    return (
+        <>
+            <h1>paul</h1>
+            <p className={styles.para}>hello world</p>
+        </>
+    )
+}
+
+export default Form
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import Field from './../components/signIn/Field.tsx'
+import Form from './../components/signIn/Form.tsx'
 
 const Product = () => {
     return (
         <>
             <h1>Sign in</h1>
             <Field />
+            <Form />
         </>
     )
 }
