bef14679edd58d75c40dde6ca8668e2e0a4c6a82
Theo Grivel <grivel.theo@protonmail.com>
2023-06-07 19:16:59 +0200

    get the input numberField
    I had to specify any

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,3 +1,17 @@
 const output = document.querySelector('#scriptOutput')
+const numberField: any = document.querySelector('#guessNumber')
+const buttonSubmit = document.querySelector('#submitButton')
+
+function handleSubmit(): void {
+    console.log(numberField.value)
+    console.log('handle submit + ')
+}
+
+console.log(numberField.value)
+console.log(buttonSubmit)
+
+buttonSubmit.addEventListener('click', handleSubmit)
+
+const numberToGuess: number = 42
 
 output.textContent = 'Hello, from the script'
