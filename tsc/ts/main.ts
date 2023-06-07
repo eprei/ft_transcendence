d2c788ed20f9e4eb3efa1b6d0b2c859fccd4047e
Theo Grivel <grivel.theo@protonmail.com>
2023-06-08 00:05:49 +0200

    logic game

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -2,16 +2,22 @@ const output = document.querySelector('#scriptOutput')
 const numberField: any = document.querySelector('#guessNumber')
 const buttonSubmit = document.querySelector('#submitButton')
 
-function handleSubmit(): void {
-    console.log(numberField.value)
-    console.log('handle submit + ')
+const numberToGuess: number = 42
+let numberList: number[]
+
+function getNumberField(): number {
+    return numberField.valueAsNumber
 }
 
-console.log(numberField.value)
-console.log(buttonSubmit)
+function handleSubmit(): void {
+    const numberField: number = getNumberField()
+
+    if (numberField < numberToGuess) output.textContent = 'too low'
+    else if (numberField > numberToGuess) output.textContent = 'too high'
+    else if (numberField === numberToGuess) output.textContent = 'win'
+    numberList.push(numberField)
+
+    console.log(numberList)
+}
 
 buttonSubmit.addEventListener('click', handleSubmit)
-
-const numberToGuess: number = 42
-
-output.textContent = 'Hello, from the script'
