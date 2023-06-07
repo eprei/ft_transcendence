83e74f353dbf6c18e72162a43e62db95a1a94e06
Theo Grivel <grivel.theo@protonmail.com>
2023-06-07 12:14:53 +0200

    print array

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,7 +1,15 @@
+function printArray(listNumbers: number[]): void {
+    listNumbers.forEach((element) => console.log('list number: ' + element))
+}
+
 function addNumbers(a: number, b: number): number {
     return a + b
 }
 
 var sum: number = addNumbers(10, 15)
+
+var myListNumbers: number[] = [0, 10, 200]
+
+printArray(myListNumbers)
 
 console.log('Sum of the two numbers is: ' + sum)
