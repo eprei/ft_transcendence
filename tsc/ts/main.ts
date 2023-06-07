6bde373a6fbbd31f36770f15a622da8c80c7bf03
Theo Grivel <grivel.theo@protonmail.com>
2023-06-08 00:58:32 +0200

    counter try

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -4,12 +4,15 @@ const buttonSubmit = document.querySelector('#submitButton')
 
 const numberToGuess: number = 42
 let numberList: number[] = []
+let counterTry: number = 0
 
 function getNumberField(): number {
     return numberField.valueAsNumber
 }
 
 function handleSubmit(): void {
+    ++counterTry
+
     const numberField: number = getNumberField()
 
     if (numberField < numberToGuess) output.textContent = 'too low'
@@ -18,6 +21,7 @@ function handleSubmit(): void {
     numberList.push(numberField)
 
     console.log(numberList)
+    console.log('try number ' + counterTry)
 }
 
 buttonSubmit.addEventListener('click', handleSubmit)
