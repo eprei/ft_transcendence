c1ef4ebe3f77c94f46cb0bc26c5e2a575eb20b97
Theo Grivel <tgrivel@c2r2s6.42lausanne.ch>
2023-06-09 11:16:40 +0200

    create function main()

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,19 +1,23 @@
-const paraJoke: HTMLParagraphElement | null =
-    document.querySelector('#paraJoke')
+function main(): void {
+    const paraJoke: HTMLParagraphElement | null =
+        document.querySelector('#paraJoke')
 
-if (paraJoke == null) throw 'Error: documentQuerySelector'
+    if (paraJoke == null) throw 'Error: documentQuerySelector'
 
-paraJoke.textContent = 'waiting the joke...'
+    paraJoke.textContent = 'waiting the joke...'
 
-const url: string = 'https://v2.jokeapi.dev/joke/Programming?type=single'
+    const url: string = 'https://v2.jokeapi.dev/joke/Programming?type=single'
 
-fetch(url)
-    .then((myResponse) => {
-        if (!myResponse.ok) {
-            throw `HTTP: fetch not OK: ${myResponse.status}`
-        }
-        return myResponse.json()
-    })
-    .then((myJson) => {
-        paraJoke.textContent = myJson.joke
-    })
+    fetch(url)
+        .then((myResponse) => {
+            if (!myResponse.ok) {
+                throw `HTTP: fetch not OK: ${myResponse.status}`
+            }
+            return myResponse.json()
+        })
+        .then((myJson) => {
+            paraJoke.textContent = myJson.joke
+        })
+}
+
+main()
