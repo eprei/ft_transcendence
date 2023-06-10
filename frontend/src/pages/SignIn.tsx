9b5524a1fd49805c3b97144a7ca42b1088ee4f45
|°_°| <grivel.theo@protonmail.com>
2023-06-10 12:03:18 +0200

    front: simple form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,19 @@
 const Product = () => {
-    return <h1>Sign in</h1>
+    return (
+        <>
+            <h1>Sign in</h1>
+            <form>
+                <label>speudo:</label>
+                <input id="pseudo" name="pseudo" required />
+                <input
+                    id="submitSpeudo"
+                    name="submitSpeudo"
+                    type="submit"
+                    value="submit speudo"
+                />
+            </form>
+        </>
+    )
 }
 
 export default Product
