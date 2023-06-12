8e479732fbc8b7f947d09bbdc5fb094be6362da6
|°_°| <grivel.theo@protonmail.com>
2023-06-12 14:28:57 +0200

    controller speudo
    method:
    - POST
    - GET

--- backend/src/speudo/speudo.controller.ts
+++ backend/src/speudo/speudo.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Post, Body } from '@nestjs/common'
+
+export class CreateSpeudo {
+    speudo: string
+}
 
 @Controller('speudo')
-export class SpeudoController {}
+export class SpeudoController {
+    @Get()
+    findAll(): string {
+        return 'get on /speudo'
+    }
+
+    @Post()
+    async create(@Body() newSpeudo: CreateSpeudo) {
+        console.log(newSpeudo)
+        return 'post on /speudo'
+    }
+}
