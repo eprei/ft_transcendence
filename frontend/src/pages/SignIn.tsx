e57a6e6db46caf6870cd5f10f21e44aa8d39153a
|°_°| <grivel.theo@protonmail.com>
2023-06-12 15:05:21 +0200

    front: form submit with POST

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,10 +1,41 @@
+import { useState } from 'react'
+
+export class Speudo {
+    speudo: string
+}
+
 const Product = () => {
+    const [speudo, setSpeudo] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        let newSpeudo: Speudo = new Speudo()
+        newSpeudo.speudo = speudo
+
+        const response = await fetch('http://localhost:8080/api/speudo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newSpeudo),
+        })
+
+        if (!response.ok) {
+            throw 'fail fetch(post)'
+        }
+    }
+
     return (
         <>
             <h1>Sign in</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>speudo:</label>
-                <input id="pseudo" name="pseudo" required />
+                <input
+                    id="pseudo"
+                    name="pseudo"
+                    onChange={(e) => setSpeudo(e.target.value)}
+                    required
+                />
                 <input
                     id="submitSpeudo"
                     name="submitSpeudo"
