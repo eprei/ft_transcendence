8412d37f484ce0b1533f176141f8f257121348c6
|°_°| <grivel.theo@protonmail.com>
2023-06-14 12:03:25 +0200

    create an infinite loop

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,29 +1,6 @@
-function printNumber(number: number): void {
-    console.log('number :' + number)
+function loop(time: number): void {
+    console.log(time)
+    window.requestAnimationFrame(loop)
 }
 
-function sumArray(listNumbers: number[]): number {
-    var sum: number = 0
-
-    listNumbers.forEach(printNumber)
-
-    listNumbers.forEach((item) => {
-        sum += item
-    })
-
-    return sum
-}
-
-function addNumbers(a: number, b: number): number {
-    return a + b
-}
-
-var sum: number = addNumbers(10, 15)
-
-console.log('Sum of the two numbers is: ' + sum)
-
-var myListNumbers: number[] = [0, 10, 200]
-
-sum = sumArray(myListNumbers)
-
-console.log('Sum of the array numbers is: ' + sum)
+window.requestAnimationFrame(loop)
