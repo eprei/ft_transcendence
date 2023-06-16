5957a111e8fc1953177cfb3e89ec18ce4739b09e
Theo Grivel <tgrivel@c3r6s3.42lausanne.ch>
2023-06-16 16:22:26 +0200

    get ft token

--- 42api.sh
+++ 42api.sh
@@ -1,3 +1,5 @@
+FILE_TOKEN_INFO="token_info.json"
+
 check_environment_variables () {
 	if [ -z "$FT_UID" ]
 	then
@@ -11,10 +13,32 @@ check_environment_variables () {
 	fi
 }
 
+get_ft_token () {
+	if [ ! -z "$FT_TOKEN" ]
+	then
+		return
+	fi
+
+	if [ -e "${FILE_TOKEN_INFO}" ]
+	then
+		echo file already exist
+	else
+		echo Download "${FILE_TOKEN_INFO}"
+		curl \
+		  --request POST \
+		  --data "grant_type=client_credentials" \
+		  --data "client_id=${FT_UID}" \
+		  --data "client_secret=${FT_SECRET}" \
+		  https://api.intra.42.fr/oauth/token \
+		  > "$FILE_TOKEN_INFO"
+	fi
+
+	export FT_TOKEN=$(jq .access_token "${FILE_TOKEN_INFO}" | tr -d '"''"')
+}
+
 main () {
 	check_environment_variables
 	get_ft_token
-
 }
 
 main
