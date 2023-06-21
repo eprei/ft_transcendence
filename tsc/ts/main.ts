b33dda8ce6fdb1cdeda7721806dcc59b18d1d475
|°_°| <grivel.theo@protonmail.com>
2023-06-21 13:57:49 +0200

    use ts class

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,6 +1,21 @@
-let canvas: HTMLCanvasElement = document.getElementById(
-    'boardGame'
-) as HTMLCanvasElement
-let ctx: CanvasRenderingContext2D = canvas.getContext('2d')
-ctx.fillStyle = 'green'
-ctx.fillRect(20, 10, 150, 100)
+class BoardGame {
+    private canvas: HTMLCanvasElement
+    private ctx: CanvasRenderingContext2D
+
+    constructor() {
+        let canvas = document.getElementById('boardGame') as HTMLCanvasElement
+        let ctx = canvas.getContext('2d')
+
+        this.canvas = canvas
+        this.ctx = ctx
+
+        this.drawGreenRectangle()
+    }
+
+    private drawGreenRectangle() {
+        this.ctx.fillStyle = 'green'
+        this.ctx.fillRect(20, 10, 150, 100)
+    }
+}
+
+new BoardGame()
