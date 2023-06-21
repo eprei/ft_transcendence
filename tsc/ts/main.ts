4bc6d7396f94d59b8ef008e0f2c6ecaf78389621
|°_°| <grivel.theo@protonmail.com>
2023-06-21 14:19:11 +0200

    create GreenRectangle class

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,11 +1,8 @@
-class BoardGame {
+class GreenRectangle {
     private canvas: HTMLCanvasElement
     private ctx: CanvasRenderingContext2D
 
-    constructor() {
-        let canvas = document.getElementById('boardGame') as HTMLCanvasElement
-        let ctx = canvas.getContext('2d')
-
+    constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
         this.canvas = canvas
         this.ctx = ctx
 
@@ -15,6 +12,22 @@ class BoardGame {
     private drawGreenRectangle() {
         this.ctx.fillStyle = 'green'
         this.ctx.fillRect(20, 10, 150, 100)
+    }
+}
+
+class BoardGame {
+    private canvas: HTMLCanvasElement
+    private ctx: CanvasRenderingContext2D
+    private greenRectangle: GreenRectangle
+
+    constructor() {
+        let canvas = document.getElementById('boardGame') as HTMLCanvasElement
+        let ctx = canvas.getContext('2d')
+
+        this.canvas = canvas
+        this.ctx = ctx
+
+        this.greenRectangle = new GreenRectangle(this.canvas, this.ctx)
     }
 }
 
