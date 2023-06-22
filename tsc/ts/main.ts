327e4fe8a226bd6a86ac7786f12af4fdb1fbfc22
|°_°| <grivel.theo@protonmail.com>
2023-06-22 20:17:07 +0200

    remove canva variable

--- tsc/ts/main.ts
+++ tsc/ts/main.ts
@@ -1,9 +1,7 @@
 class Ball {
-    private canvas: HTMLCanvasElement
     private ctx: CanvasRenderingContext2D
 
-    constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
-        this.canvas = canvas
+    constructor(ctx: CanvasRenderingContext2D) {
         this.ctx = ctx
 
         this.draw()
@@ -17,20 +15,13 @@ class Ball {
 }
 
 class Rectangle {
-    private canvas: HTMLCanvasElement
     private ctx: CanvasRenderingContext2D
     private _x: number
     private _y: number
     private _heigth: number
     private _width: number
 
-    constructor(
-        canvas: HTMLCanvasElement,
-        ctx: CanvasRenderingContext2D,
-        width: number,
-        heigth: number
-    ) {
-        this.canvas = canvas
+    constructor(ctx: CanvasRenderingContext2D, width: number, heigth: number) {
         this.ctx = ctx
 
         this._x = 20
@@ -56,7 +47,6 @@ class Rectangle {
 }
 
 class BoardGame {
-    private canvas: HTMLCanvasElement
     private ctx: CanvasRenderingContext2D
     private rectangle: Rectangle
     private ball: Ball
@@ -66,11 +56,10 @@ class BoardGame {
         let canvas = document.getElementById('boardGame') as HTMLCanvasElement
         let ctx = canvas.getContext('2d')
 
-        this.canvas = canvas
         this.ctx = ctx
 
-        this.rectangle = new Rectangle(this.canvas, this.ctx, 150, 100)
-        this.ball = new Ball(this.canvas, this.ctx)
+        this.rectangle = new Rectangle(this.ctx, 150, 100)
+        this.ball = new Ball(this.ctx)
 
         this._lastTime = 0
         this.loop(0)
