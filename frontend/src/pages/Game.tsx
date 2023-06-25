6dde3a1c8f5a1f04fc6d5badff26e81d49a12d09
|°_°| <grivel.theo@protonmail.com>
2023-06-25 14:18:59 +0200

    use state to save frame information

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,6 +1,22 @@
 import styles from './Game.module.css'
+import { useState } from 'react'
 
 const Game = () => {
+    const [frame, setFrame] = useState({
+        paddle: {
+            position: {
+                x: 10,
+                y: 20,
+            },
+            size: {
+                width: 10,
+                height: 50,
+            },
+        },
+    })
+
+    console.log(frame)
+
     return (
         <>
             <h1>The Game</h1>
