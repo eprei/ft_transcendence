4d2394e1aefaf4fe4d37d06a1f7466ae2d8a144b
|°_°| <grivel.theo@protonmail.com>
2023-06-25 12:59:55 +0200

    create a black canva in the middle

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,7 +1,10 @@
+import styles from './Game.module.css'
+
 const Game = () => {
     return (
         <>
             <h1>The Game</h1>
+            <canvas className={styles.boarGame}></canvas>
         </>
     )
 }
