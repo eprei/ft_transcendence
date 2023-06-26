a087ab82b8f42ff4d843e286d0cf1a8bc1cda8e3
Theo Grivel <tgrivel@c3r4s1.42lausanne.ch>
2023-06-26 15:26:06 +0200

    wip try to connect to the backend
    resource
    https://socket.io/how-to/use-with-react#remarks-about-the-useeffect-hook
    
    I'm blocked with this message in the
    console of devtool
    ```
    Blocage d’une requête multiorigine
    (Cross-Origin Request) : la politique «
    Same Origin » ne permet pas de consulter la
    ressource distante située sur
    http://localhost/socket.io/?EIO=4&transport=polling&t=OZt_kpQ.
    Raison : échec de la requête CORS. Code
    d’état : (null).
    ```

--- frontend/src/components/Pong/Ws.tsx
+++ frontend/src/components/Pong/Ws.tsx
@@ -1,5 +1,47 @@
+import { socket } from './socket'
+import { useState, useEffect } from 'react'
+
+import ConnectionState from './ConnectionState'
+//import Events from './Events'
+//import ConnectionManager from './ConnectionManager'
+//import MyForm from './MyForm'
+
 const Ws = () => {
-    return <h3>Websocket</h3>
+    const [isConnected, setIsConnected] = useState(socket.connected)
+    const [fooEvents, setFooEvents] = useState([])
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true)
+        }
+
+        function onDisconnect() {
+            setIsConnected(false)
+        }
+
+        function onFooEvent(value) {
+            setFooEvents((previous) => [...previous, value])
+        }
+
+        socket.on('connect', onConnect)
+        socket.on('disconnect', onDisconnect)
+        socket.on('foo', onFooEvent)
+
+        return () => {
+            socket.off('connect', onConnect)
+            socket.off('disconnect', onDisconnect)
+            socket.off('foo', onFooEvent)
+        }
+    }, [])
+
+    return (
+        <>
+            <ConnectionState isConnected={isConnected} />
+        </>
+    )
+//            <Events events={fooEvents} />
+//            <ConnectionManager />
+//            <MyForm />
 }
 
 export default Ws
