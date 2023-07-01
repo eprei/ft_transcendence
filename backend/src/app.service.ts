3e9c0d5ae83fee1302f38d58c618602c13f1a467
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-01 19:17:18 +0200

    seed for player ok, email column deleted

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -13,15 +13,15 @@ export class AppService {
         const player1 = this.playerRepo.create({login: 'Boss', avatarUrl: 'http://localhost:8080/api/player/picture/profil-picture-placeholder.png'})
         await this.playerRepo.save(player1)
         const player2 = this.playerRepo.create({login: 'epresa-c', avatarUrl: 'http://localhost:8080/api/player/picture/epresa-c.jgp'})
-        await this.playerRepo.save(player1)
+        await this.playerRepo.save(player2)
         const player3 = this.playerRepo.create({login: 'mpons', avatarUrl: 'http://localhost:8080/api/player/picture/mpons.jpg'})
-        await this.playerRepo.save(player1)
+        await this.playerRepo.save(player3)
         const player4 = this.playerRepo.create({login: 'rburri', avatarUrl: 'http://localhost:8080/api/player/picture/rburri.jpg'})
-        await this.playerRepo.save(player1)
+        await this.playerRepo.save(player4)
         const player5 = this.playerRepo.create({login: 'sbars', avatarUrl: 'http://localhost:8080/api/player/picture/sbars.jpg'})
-        await this.playerRepo.save(player1)
+        await this.playerRepo.save(player5)
         const player6 = this.playerRepo.create({login: 'tgrivel', avatarUrl: 'http://localhost:8080/api/player/picture/tgrivel.jpg'})
-        await this.playerRepo.save(player1)
+        await this.playerRepo.save(player6)
     }
     getHello(): string {
         return 'Hello World!!!'
--- backend/src/typeorm/player.entity.ts
+++ backend/src/typeorm/player.entity.ts
@@ -2,21 +2,13 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class Player {
-    @PrimaryGeneratedColumn({
-        type: 'int',
-        name: 'player_id',
-    })
+    @PrimaryGeneratedColumn()
     id: number
 
     @Column({
-        type: 'varchar',
-        length: 100,
         unique: true,
     })
     login: string
-
-    @Column()
-    email: string
 
     @Column()
     avatarUrl: string
