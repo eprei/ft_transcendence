14a8e02c80b4fda4d369e51c2ba1ad00fef09f36
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-02 17:31:31 +0200

    match dto create

--- backend/src/match/dto/create-match.dto.ts
+++ backend/src/match/dto/create-match.dto.ts
@@ -1,1 +1,24 @@
-export class CreateMatchDto {}
+import { IsNotEmpty, IsOptional } from 'class-validator'
+
+export class CreateMatchDto {
+    @IsOptional()
+    id: number
+
+    @IsNotEmpty()
+    playerHome: number
+
+    @IsNotEmpty()
+    playerForeign: number
+
+    @IsNotEmpty()
+    winner: number
+
+    @IsNotEmpty()
+    homeScore: number
+
+    @IsNotEmpty()
+    foreignScore: number
+
+    @IsOptional()
+    timestamp: Date
+}
