37fb0d6cf8593eaae28ec4b493f6a6955cc6907a
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2023-07-05 11:47:20 +0200

    backend: add contents
    resource:
    https://www.joshmorony.com/creating-a-simple-live-chat-server-with-nestjs-websockets/#1-creating-the-nestjs-server

--- backend/src/pong/pong/pong.gateway.ts
+++ backend/src/pong/pong/pong.gateway.ts
@@ -8,8 +8,30 @@ import {
 
 @WebSocketGateway({ path: '/pong' })
 export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server
+    users: number = 0
+
+    async handleConnection() {
+        console.log('handleConnection')
+        // A client has connected
+        this.users++
+
+        // Notify connected clients of current users
+        this.server.emit('users', this.users)
+    }
+
+    async handleDisconnect() {
+        console.log('handleDisconnect')
+        // A client has disconnected
+        this.users--
+
+        // Notify connected clients of current users
+        this.server.emit('users', this.users)
+    }
+
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): string {
+        console.log('SubscribeMessage')
         return 'Hello world!'
     }
 }
