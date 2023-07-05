424ba6ecef8be92a00c1c383e67cf62ec8dc95b3
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2023-07-05 11:34:14 +0200

    wip

--- backend/src/pong/pong/pong.gateway.ts
+++ backend/src/pong/pong/pong.gateway.ts
@@ -1,7 +1,13 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
+import {
+    WebSocketGateway,
+    WebSocketServer,
+    SubscribeMessage,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+} from '@nestjs/websockets'
 
 @WebSocketGateway({ path: '/pong' })
-export class PongGateway {
+export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): string {
         return 'Hello world!'
