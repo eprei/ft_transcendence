ec762902ec4b84dd927b350baf11bc7a1283c73d
Theo Grivel <grivel.theo@protonmail.com>
2023-07-06 14:22:11 +0200

    frontend: websocket connection

--- frontend/src/components/pong/Pong.tsx
+++ frontend/src/components/pong/Pong.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+
+interface Frame {
+    x: number
+    y: number
+}
+
 const Pong = () => {
+    const socket = io('http://localhost:8080')
+    const [frame, setFrame] = useState<Frame>()
+
+    useEffect(() => {
+        socket.emit('getFrame', { id: 50 }, (response: Frame) => {
+            setFrame(response)
+        })
+    }, [])
+
+    console.log('actual frame: ', JSON.stringify(frame))
+
     return <p>the pong game !</p>
 }
 
