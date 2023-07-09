d7254abf945233a524ed197961fd0b2b09a248df
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-09 19:39:04 +0200

    add Chat.module.css

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,16 @@
+import styles from './Chat.module.css'
+import ChannelBox from '../components/chat/channelBox/ChannelBox.tsx'
+import ChatBox from '../components/chat/chatBox/ChatBox'
+import UserList from '../components/chat/userBox/UserBox.tsx'
+
 const Chat = () => {
     return (
         <>
-            <h1>Chat</h1>
+            <div className={styles.chatContainer}>
+                <ChannelBox />
+                <ChatBox />
+                <UserList />
+            </div>
         </>
     )
 }
